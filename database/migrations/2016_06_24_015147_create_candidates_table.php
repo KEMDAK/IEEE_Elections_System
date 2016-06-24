055470d694259a980c12c69654c61e9a883e40f2
waheed94 <mohammed.waheed44@gmail.com>
2016-06-24 04:55:54 +0200

    candidate model finished & migrated

---
+++ database/migrations/2016_06_24_015147_create_candidates_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('position');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('major');
+            $table->integer('graduation_year');
+            $table->longText('description');  
+            $table->string('image_url');
+            $table->integer('votes');     
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('candidates');
+    }
+}
