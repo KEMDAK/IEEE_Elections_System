87827715cedb33a601abbbe4fe1193ab95af79cb
Kareem Mokhtar <k.m.a1_2_3@hotmail.com>
2016-06-24 17:28:45 +0200

    finished the user model and seting up the mail configuration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,4 @@ Route::get('/', function () {
 });
 
 Route::auth();
+Route::post('activate', 'Auth\AuthController@activate');
--- app/User.php
+++ app/User.php
@@ -12,7 +12,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'ieee_membership_id', 'role', 'email',
+        'password', 'active', 'status',
     ];
 
     /**
@@ -23,4 +24,40 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Returns true if this user is an admin.
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return strcmp($this->role,"Admin") == 0;
+    }
+
+    /**
+     * Returns true if this user is a voter.
+     * @return bool
+     */
+    public function isVoter()
+    {
+        return strcmp($this->role,"Voter") == 0;
+    }
+
+    /**
+     * Returns true if this user is active and can sign in.
+     * @return bool
+     */
+    public function isActive()
+    {
+        return strcmp($this->active,"true") == 0;
+    }
+
+    /**
+     * Returns true if this user has voted before.
+     * @return bool
+     */
+    public function hasVoted()
+    {
+        return strcmp($this->status,"true") == 0;
+    }
 }
