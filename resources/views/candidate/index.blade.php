0b9f861658beb935e5d2dccc45ec91a9a4b8133a
Muhammad Khattab <msskmohammed96@gmail.com>
2016-06-27 00:40:16 +0200

    finished candidate.index

--- resources/views/candidate/index.blade.php
+++ resources/views/candidate/index.blade.php
@@ -4,30 +4,27 @@
 
 <div class = "container">
 
-  <div class="row">
-    <div class="row position">
-      <span class=" col-md-3 label label-primary">President</span>
-    </div>
-
     <!-- candidates for president's position pictures and names-->
-    @foreach ($presidents as $candidate)
-      @unless(strcmp($candidate->position,'Empty Position') == 0)
-        <div class="candidate-pic">
-            <a href = {{ url('candidate', $candidate->id)}}>
-              <img src={{$candidate->image_url}} class="img-rounded" alt={{$candidate->first_name}} width="300" height="300">
-              <div class="candidate-name">{{$candidate->first_name}} {{$candidate->last_name}}</div>
-            </a>
-        </div>
-      @endunless
-    @endforeach
-  </div>
-
-  <div class="row">
-    <div class="row position">
-      <span class=" col-md-3 label label-primary">Vice President</span>
+    <div class="panel panel-default">
+      <div class="panel-heading">Applying for President's position</div>
+      <div class="panel-body">
+        @foreach ($presidents as $candidate)
+          @unless(strcmp($candidate->position,'Empty Position') == 0)
+            <div class="candidate-pic">
+              <a href = {{ url('candidate', $candidate->id)}}>
+                <img src={{$candidate->image_url}} class="img-rounded" alt={{$candidate->first_name}} width="300" height="300">
+                <div class="candidate-name">{{$candidate->first_name}} {{$candidate->last_name}}</div>
+              </a>
+            </div>
+          @endunless
+        @endforeach
+      </div>
     </div>
 
     <!-- candidates for vice president's position pictures and names-->
+    <div class="panel panel-default">
+      <div class="panel-heading">Applying for Vice President's position</div>
+      <div class="panel-body">
     @foreach ($vice_presidents as $candidate)
       @unless(strcmp($candidate->position,'Empty Position') == 0)
         <div class="candidate-pic">
@@ -39,13 +36,12 @@
       @endunless
     @endforeach
   </div>
-
-  <div class="row">
-    <div class="row position">
-      <span class=" col-md-3 label label-primary">Secretary</span>
-    </div>
+</div>
 
     <!-- candidates for secratory's position pictures and names-->
+    <div class="panel panel-default">
+      <div class="panel-heading">Applying for Secratory's position</div>
+      <div class="panel-body">
     @foreach ($secratories as $candidate)
       @unless(strcmp($candidate->position,'Empty Position') == 0)
         <div class="candidate-pic">
@@ -57,13 +53,12 @@
       @endunless
     @endforeach
   </div>
-
-  <div class="row">
-    <div class="row position">
-      <span class=" col-md-3 label label-primary">Treassurer</span>
-    </div>
+</div>
 
     <!-- candidates for treasurer's position pictures and names-->
+    <div class="panel panel-default">
+      <div class="panel-heading">Applying for Treassurer's position</div>
+      <div class="panel-body">
     @foreach ($treasurers as $candidate)
       @unless(strcmp($candidate->position,'Empty Position') == 0)
         <div class="candidate-pic">
@@ -75,7 +70,7 @@
       @endunless
     @endforeach
   </div>
-
+</div>
 </div>
 
 @endsection
