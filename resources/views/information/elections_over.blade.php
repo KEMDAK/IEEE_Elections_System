17793d7e6dfa08b5fbc05cde7dda10af2be1a647
Muhammad Khattab <msskmohammed96@gmail.com>
2016-07-02 03:16:52 +0200

    edited elections_over

--- resources/views/information/elections_over.blade.php
+++ resources/views/information/elections_over.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
   <div class="container">
-    <h2 class = "one-liner">Elections are over.</h2>
-    <h2>The results will be available soon.</h2>
+    <h2 class = "one-liner">Elections are over.<br>The results will be available soon.</h2>
   </div>
 @endsection
