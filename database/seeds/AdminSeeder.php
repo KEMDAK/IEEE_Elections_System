946c58ff80a266fe9ed056ea4a2adcd6b613154a
Kareem Mokhtar <k.m.a1_2_3@hotmail.com>
2016-07-03 00:36:35 +0200

    added the admin accounts

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -16,9 +16,25 @@ class AdminSeeder extends Seeder
         User::create([
             'ieee_membership_id' => '1',
             'role' => 'Admin',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('admin'),
+            'email' => 'dr.amr@admin.com',
+            'password' => bcrypt('damrr.admin'),
             'active' => '1',
+            'status' => '1'
+        ]);
+
+        User::create([
+            'ieee_membership_id' => '92849112',
+            'role' => 'Admin',
+            'email' => 'sherifmagdyabdelhalim@gmail.com',
+            'active' => '0',
+            'status' => '1'
+        ]);
+
+        User::create([
+            'ieee_membership_id' => '93390793',
+            'role' => 'Admin',
+            'email' => 'walidtarekelhefny@gmail.com',
+            'active' => '0',
             'status' => '1'
         ]);
     }
