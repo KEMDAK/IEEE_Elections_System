82d13b68a158a8821cb345dbe8ce2bf5d6af39ac
Kareem Mokhtar <k.m.a1_2_3@hotmail.com>
2016-07-03 22:06:51 +0200

    fixed the logos position

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,42 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<img id = "welcome-img" src="img/leaves.jpg" alt="leaves" width="100%" height="100%">
 
 <div class="container">
+    <center>
 
-    <div class="row">
-      <img src="img/elections_light.png" alt = "elections" id = "welcome-logo">
-    </div>
+        <div class="row">
+            <img src="img/elections_c.png" alt = "elections" width="100%">
+        </div>
 
-<div id = "welcome-comp">
-    <h5 class = "row">
-      <span class "col-md-12" id = "welcome-text">
-        Upgrading Humanity!
-      </span>
-    </h5>
-    <div class="row">
-        <span class "col-md-4">
-          <a href="{{ url('/job_description') }}"><button type="button" class="btn btn-primary">Job Description</button></a>
-        </span>
+        <br><br>
 
-        <span class "col-md-4">
-          <a href="{{ url('/eligibility_conditions') }}"><button type="button" class="btn btn-primary">Eligibility Conditions</button></a>
-        </span>
+        <div class="row">
+            <span class "col-md-4">
+                <a href="{{ url('/job_description') }}"><button type="button" class="btn btn-primary">Job Description</button></a>
+            </span>
 
-        <span class "col-md-4">
-          <a href="{{ url('/election_phases') }}"><button type="button" class="btn btn-primary">How to apply</button></a>
-        </span>
-    </div>
+            <span class "col-md-4">
+                <a href="{{ url('/eligibility_conditions') }}"><button type="button" class="btn btn-primary">Eligibility Conditions</button></a>
+            </span>
 
-    @if ((!Auth::guest()) && Auth::user()->isVoter())
-    <div class="row">
-      <span class "col-md-12">
-        <a href="{{ url('/vote') }}"><button type="button" class="btn btn-primary">Vote</button></a>
-      </span>
-    </div>
-    @endif
-  </div>
-
+            <span class "col-md-4">
+                <a href="{{ url('/election_phases') }}"><button type="button" class="btn btn-primary">How to apply</button></a>
+            </span>
+        </div>
+    </center>
 </div>
 @endsection
