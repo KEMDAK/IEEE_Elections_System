de0976babff61bc28156e69f65b37180501e6cca
Kareem Mokhtar <k.m.a1_2_3@hotmail.com>
2016-07-06 02:08:11 +0200

    added the example structure for the results partial and finished the backend for the results

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -245,7 +245,11 @@
             background-color: #174590;
         }
     }
-
+    .progress{
+        height: 50px;
+        box-shadow: none;
+        background-color: transparent;
+    }
 
     </style>
 </head>
@@ -300,43 +304,6 @@
         @include('flash::message')
     </div>
     @yield('content')
-
-    <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-
-    <script>
-    function progress(percent, $element) {
-      $element.animate({ width: percent }, 700);
-    }
-
-    function results_text() {
-      $('.results-text').animate({marginLeft: "30px"},1000)
-    }
-
-    $(document).ready( function() {
-      $('div.alert').not('.alert-important').delay(3000).slideUp(300);
-
-      $('#flash-overlay-modal').modal();
-
-      var chairman_votes = "60%";
-      progress(chairman_votes, $('#chairman-votes'));
-
-      var vice_chairman_votes = "60%";
-      progress(vice_chairman_votes, $('#vice_chairman-votes'));
-
-      var secretary_votes = "60%";
-      progress(secretary_votes, $('#secretary-votes'));
-
-      var treasurer_votes = "60%";
-      progress(treasurer_votes, $('#treasurer-votes'));
-
-      results_text();
-
-    });
-    </script>
-
 
 </body>
 </html>
--- resources/views/vote/results_partial.blade.php
+++ resources/views/vote/results_partial.blade.php
@@ -1,27 +1,23 @@
-    <div class = "row">
-      <h3>
+<div class = "row">
+    <h3>
         {{ $position }}
-      </h3>
-    </div>
+    </h3>
+</div>
 
+<div class="candidates_results">
     <div class = "row">
 
-      <div class = "col-md-4">
-        <img class = "results-pic" src = {{url( $winner->image_url)}} alt = "winner">
-      </div>
-
-      <div class = "col-md-3">
-        <h3 class = "results-text">
-          {{ $winner->name }}
-        </h3>
-      </div>
-
-    </div>
-
-    <div class = "row">
-      <div class="progress">
-        <div class="progress-bar" role="progressbar"
-        aria-valuemin="0" aria-valuemax="100" id = {{ $id }}>
+        <div class = "col-md-1 col-xs-1">
+            <img class = "results-pic" src = {{url( $winner->image_url) }} alt = "winner" width="50px" height="50px">
         </div>
-      </div>
+
+        <div class = "col-md-1 col-md-offset-0 col-xs-offset-1 col-xs-3" style="font-size: 15px; padding-top: 4px">
+            {{ $winner->first_name }} {{ $winner->last_name }}
+        </div>
+
+        <div class="progress">
+            <div class="progress-bar" role="progressbar"
+            aria-valuemin="0" aria-valuemax="100" id = {{ $id }}></div>
+        </div>
     </div>
+</div>
